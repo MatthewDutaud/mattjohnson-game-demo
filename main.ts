7e4a25866f915ad140f5c1b29861121cfbc0ad79
JohnsonDuong <71087545+JohnsonDuong@users.noreply.github.com>
2020-09-10 13:06:05 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,93 +1,17 @@
 enum ActionKind {
     Walking,
     Idle,
-    Jumping
+    Jumping,
+    Swimming
 }
 namespace SpriteKind {
     export const Ghost = SpriteKind.create()
 }
-controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    mySprite,
-    [img`
-        . . . . . . . . c c c c . . . . 
-        . . . . . . c c d d d d c . . . 
-        . . . . . c c c c c c d c . . . 
-        . . . . c c 4 4 4 4 d c c . c c 
-        . . . c 4 d 4 4 4 4 4 1 c c 4 c 
-        . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
-        . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
-        f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
-        f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
-        f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
-        . f 4 4 4 4 1 c c 4 4 d f f . . 
-        . . f f 4 d 4 4 4 4 4 c f . . . 
-        . . . . f f 4 4 4 4 c d b c . . 
-        . . . . . . f f f f d d d c . . 
-        . . . . . . . . . . c c c . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c c c . . . . 
-        . . . . . . c c d d d d c . . . 
-        . . . . . c c c c c c d c . . . 
-        . . . . c c 4 4 4 4 d c c . . . 
-        . . . c 4 d 4 4 4 4 4 1 c . c c 
-        . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
-        . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
-        f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
-        f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
-        f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
-        . f 4 4 4 4 1 c 4 f 4 d f f f f 
-        . . f f 4 d 4 4 f f 4 c f c . . 
-        . . . . f f 4 4 4 4 c d b c . . 
-        . . . . . . f f f f d d d c . . 
-        . . . . . . . . . . c c c . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . c c c c . . . 
-        . . . . . . . c c d d d d c . . 
-        . . . . . c c c c c c d d c . . 
-        . . . c c c 4 4 4 4 d c c c c c 
-        . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
-        . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
-        f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
-        f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
-        f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
-        . f 4 4 4 4 1 4 4 f 4 4 d f . . 
-        . . f 4 4 1 4 c c 4 4 d f . . . 
-        . . . f d 4 4 4 4 4 4 c f . . . 
-        . . . . f f 4 4 4 4 c d b c . . 
-        . . . . . . f f f f d d d c . . 
-        . . . . . . . . . . c c c . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . c c c c c . . . . 
-        . . . . . . c d d d d d c . . . 
-        . . . . . . c c c c c d c . . . 
-        . . . . . c 4 4 4 4 d c c . . . 
-        . . . . c d 4 4 4 4 4 1 c . . . 
-        . . . c 4 4 1 4 4 4 4 4 1 c . . 
-        . . c 4 4 4 4 1 4 4 4 4 1 c c c 
-        . c 4 4 4 4 4 1 c c 4 4 1 4 4 c 
-        . c 4 4 4 4 4 1 4 4 f 4 1 f 4 f 
-        f 4 4 4 4 f 4 1 c 4 f 4 d f 4 f 
-        f 4 4 4 4 4 4 1 4 f f 4 f f 4 f 
-        . f 4 4 4 4 1 4 4 4 4 c b c f f 
-        . . f f f d 4 4 4 4 c d d c . . 
-        . . . . . f f f f f c c c . . . 
-        . . . . . . . . . . . . . . . . 
-        `],
-    500,
-    true
-    )
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     Shark.destroy()
     info.changeLifeBy(-1)
 })
 let Shark: Sprite = null
-let mySprite: Sprite = null
 scene.setBackgroundColor(9)
 tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040004060000050400070303030303030303030302010101010101010101`, img`
     . . . . . . . . . . 
@@ -99,7 +23,7 @@ tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000000000000000
     2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 
     `, [myTiles.transparency16,sprites.builtin.oceanSand11,sprites.builtin.oceanSand8,sprites.builtin.oceanSand2,sprites.builtin.coral5,sprites.builtin.coral1,sprites.builtin.coral4,sprites.builtin.coral0], TileScale.Sixteen))
-mySprite = sprites.create(img`
+let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . c c c c . . . . 
     . . . . . . c c d d d d c . . . 
@@ -144,45 +68,5 @@ game.onUpdateInterval(2000, function () {
         `, SpriteKind.Enemy)
     Shark.setVelocity(-50, 0)
     Shark.setPosition(160, randint(10, 90))
-    animation.runImageAnimation(
-    Shark,
-    [img`
-        .................ccfff..............
-        ................cddbbf..............
-        ...............cddbbf...............
-        .........ffffffccbbcf...............
-        ......fffbbbbbbbbcccff..............
-        .....fbbbbbbbbbbbbbbbcfff......ccccc
-        .....bcbbbbbffbcbcbbbbcccff...cdbbbc
-        .....bbb1111ffbbcbcbbbcccccffcddbbc.
-        .....fb11111111bcbcbbbcccccccbdbbf..
-        ......fccc33c11bbbbbbcccccccccbbcf..
-        .......fc131cc11bbbbccccccccffbccf..
-        ........f33c1111bbbcccccbdbc..fbbcf.
-        .........ff1111cbbbfdddddcc....fbbf.
-        ...........ccc1fbdbbfddcc.......fbbf
-        ..............ccfbdbbfc..........fff
-        .................fffff..............
-        `,img`
-        ...........fffffff...ccfff..........
-        ..........fbbbbbbbffcbbbbf..........
-        ..........fbb111bbbbbffbf...........
-        ..........fb11111ffbbbbff...........
-        ..........f1cccc1ffbbbbbcff.........
-        ..........ffc1c1c1bbcbcbcccf........
-        ...........fcc3331bbbcbcbcccf..ccccc
-        ............c333c1bbbcbcbccccfcddbbc
-        ............c333c1bbbbbbbcccccddbcc.
-        ............c333c11bbbbbccccccbbcc..
-        ...........cc331c11bbbbccccccfbccf..
-        ...........cc13c11cbbbcccccbbcfccf..
-        ...........c111111cbbbfdddddc.fbbcf.
-        ............cc1111fbdbbfdddc...fbbf.
-        ..............cccfffbdbbfcc.....fbbf
-        ....................fffff........fff
-        `],
-    200,
-    true
-    )
     Shark.setFlag(SpriteFlag.DestroyOnWall, true)
 })
