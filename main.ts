90de55181d7eae2344116f39d40ffda2cff60426
MatthewDutaud <71087302+MatthewDutaud@users.noreply.github.com>
2020-09-10 11:54:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let Shark: Sprite = null
 scene.setBackgroundColor(9)
 tiles.setTilemap(tiles.createTilemap(hex`1400080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004040000000400000400000004000000000003030303030303030303030303030303000000000201010101010101010101010101010000000000`, img`
     . . . . . . . . . . . . . . . . . . . . 
@@ -27,3 +28,25 @@ let mySprite = sprites.create(img`
     . . . . . . f f f f d d d c . . 
     . . . . . . . . . . c c c . . . 
     `, SpriteKind.Player)
+game.onUpdateInterval(700, function () {
+    Shark = sprites.create(img`
+        ....................ccfff...........
+        ..........fffffffffcbbbbf...........
+        .........fbbbbbbbbbfffbf............
+        .........fbb111bffbbbbff............
+        .........fb11111ffbbbbbcff..........
+        .........f1cccc11bbcbcbcccf.........
+        ..........fc1c1c1bbbcbcbcccf...ccccc
+        ............c3331bbbcbcbccccfccddbbc
+        ...........c333c1bbbbbbbcccccbddbcc.
+        ...........c331c11bbbbbcccccccbbcc..
+        ..........cc13c111bbbbccccccffbccf..
+        ..........c111111cbbbcccccbbc.fccf..
+        ...........cc1111cbbbfdddddc..fbbcf.
+        .............cccffbdbbfdddc....fbbf.
+        ..................fbdbbfcc......fbbf
+        ...................fffff.........fff
+        `, SpriteKind.Enemy)
+    Shark.setVelocity(-50, 0)
+    Shark.setPosition(160, randint(10, 100))
+})
